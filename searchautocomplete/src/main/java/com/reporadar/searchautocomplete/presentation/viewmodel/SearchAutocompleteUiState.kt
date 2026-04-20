67ba03d8356666bb771cdf1c9f6e93f12ad98c67
Arian Dedaj <adedaj3@gmail.com>
2026-04-20 15:43:18 +0200

    add SearchAutocompleteUiState

---
+++ searchautocomplete/src/main/java/com/reporadar/searchautocomplete/presentation/viewmodel/SearchAutocompleteUiState.kt
@@ -0,0 +1,20 @@
+package com.reporadar.searchautocomplete.presentation.viewmodel
+
+import com.reporadar.searchautocomplete.data.result.FetchDataErrorType
+import com.reporadar.searchautocomplete.domain.SearchResultItem
+
+sealed class SearchAutocompleteUiState {
+
+    data object Idle : SearchAutocompleteUiState()
+
+    data object Loading : SearchAutocompleteUiState()
+
+    data class Success(
+        val items: List<SearchResultItem>
+    ) : SearchAutocompleteUiState()
+
+    data class Failure(
+        val errorType: FetchDataErrorType
+    ) : SearchAutocompleteUiState()
+
+}

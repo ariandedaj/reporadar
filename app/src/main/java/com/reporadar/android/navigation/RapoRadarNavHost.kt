37060f736379e428d239dba3584886a77bc97e70
Arian Dedaj <adedaj3@gmail.com>
2026-04-20 12:59:25 +0200

    wip: root screen - search button

--- app/src/main/java/com/reporadar/android/navigation/RapoRadarNavHost.kt
+++ app/src/main/java/com/reporadar/android/navigation/RapoRadarNavHost.kt
@@ -1,7 +1,22 @@
 package com.reporadar.android.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.reporadar.android.view.RootScreen
+
+private const val RootScreen = "root_screen"
 
 @Composable
 fun RepoRadarNavHost() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = RootScreen
+    ) {
+        composable(route = RootScreen) {
+            RootScreen { }
+        }
+    }
 }

d93c142ad1e2d0536779d1e32b2f0dd7b9080a95
Arian Dedaj <adedaj3@gmail.com>
2026-04-20 15:01:34 +0200

    define SearchResult

---
+++ searchautocomplete/src/main/java/com/reporadar/searchautocomplete/data/result/SearchResult.kt
@@ -0,0 +1,38 @@
+package com.reporadar.searchautocomplete.data.result
+
+import com.reporadar.searchautocomplete.domain.SearchResultItem
+
+sealed class SearchResult {
+
+    data class Success(
+        val items: List<SearchResultItem>
+    ) : SearchResult()
+
+    data class Error(
+        val errorType: FetchDataErrorType
+    ) : SearchResult()
+
+}
+
+sealed class FetchDataErrorType(
+    open val exception: Exception? = null
+) {
+
+    data class NoInternet(
+        override val exception: Exception? = null
+    ) : FetchDataErrorType()
+
+    data class ParsingFailed(
+        override val exception: Exception? = null
+    ) : FetchDataErrorType()
+
+    data class HttpError(
+        override val exception: Exception? = null,
+        val httpStatusCode: Int
+    ) : FetchDataErrorType()
+
+    data class Unknown(
+        override val exception: Exception? = null
+    ) : FetchDataErrorType()
+
+}

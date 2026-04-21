f9bcee0336ac1b6509bf9f77c66aa8a5e915a446
Arian Dedaj <adedaj3@gmail.com>
2026-04-21 09:14:05 +0200

    update search field - update appearance and add the trailing icon to clear text

--- searchautocomplete/src/main/java/com/reporadar/searchautocomplete/presentation/view/SearchField.kt
+++ searchautocomplete/src/main/java/com/reporadar/searchautocomplete/presentation/view/SearchField.kt
@@ -3,6 +3,8 @@ package com.reporadar.searchautocomplete.presentation.view
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -15,6 +17,7 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import com.reporadar.searchautocomplete.R
@@ -51,12 +54,28 @@ fun SearchField(
             unfocusedContainerColor = Color.Transparent,
             disabledContainerColor = Color.Transparent,
             errorContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent,
         ),
         placeholder = {
             Text(
                 text = stringResource(id = R.string.search_placeholder_text),
                 style = MaterialTheme.typography.bodyLarge
             )
+        },
+        trailingIcon = {
+            if (searchQuery.isNotEmpty()) {
+                IconButton(
+                    onClick = { onSearchTextChange("") }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.clear_small_icon),
+                        contentDescription = stringResource(id = R.string.clear_text_icon_description)
+                    )
+                }
+            }
         }
     )
 }

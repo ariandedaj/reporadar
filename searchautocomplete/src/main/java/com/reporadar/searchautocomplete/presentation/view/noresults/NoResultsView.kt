97069a7da6c505e38a0e396ad016d106e946cc47
Arian Dedaj <adedaj3@gmail.com>
2026-04-21 09:31:39 +0200

    implement NoResultsView

---
+++ searchautocomplete/src/main/java/com/reporadar/searchautocomplete/presentation/view/noresults/NoResultsView.kt
@@ -0,0 +1,68 @@
+package com.reporadar.searchautocomplete.presentation.view.noresults
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.reporadar.searchautocomplete.R
+
+@Composable
+fun NoResultsView(
+    searchQuery: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                horizontal = 16.dp,
+                vertical = 48.dp
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.search_icon),
+                contentDescription = null,
+                modifier = Modifier.size(size = 20.dp),
+                tint = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.55f)
+            )
+
+            Spacer(modifier = Modifier.width(width = 8.dp))
+
+            Text(
+                text = stringResource(id = R.string.no_results_title),
+                style = MaterialTheme.typography.titleMedium,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+
+        Spacer(modifier = Modifier.height(height = 8.dp))
+
+        Text(
+            text = stringResource(
+                id = R.string.no_results_message,
+                searchQuery
+            ),
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}

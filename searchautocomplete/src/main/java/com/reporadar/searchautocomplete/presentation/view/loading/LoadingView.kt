d3994401f2ca953fb560d024fa685714d8af4cdc
Arian Dedaj <adedaj3@gmail.com>
2026-04-21 10:00:44 +0200

    add preview

--- searchautocomplete/src/main/java/com/reporadar/searchautocomplete/presentation/view/loading/LoadingView.kt
+++ searchautocomplete/src/main/java/com/reporadar/searchautocomplete/presentation/view/loading/LoadingView.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 private val LineWidthFractions = listOf(
@@ -44,4 +45,10 @@ fun LoadingView() {
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun LoadingPreview() {
+    LoadingView()
 }
